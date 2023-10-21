590914bc46207c99b1172dac96ca710a20bb5d5d
Parviz Pirizade <parvizpirizade@Parvizs-MacBook-Air.local>
2023-10-21 22:07:01 +0200

    comments added for the explanation of synchronized keyword

--- ClassA.java
+++ ClassA.java
@@ -8,10 +8,15 @@ public  synchronized void method() throws InterruptedException{
          System.out.print(a);
            System.out.print("\n");
            a++;
+           // this is actually this  a =  a+1
+           // but what if both of the threads  do it at the same time ?
+           // then  both of them will do  for example at some ith point
+           // 45 =44+1;
+           //  but two threads both incrementing the value, it should be 46 not 45!
+
        
     }
    
 }
 
 }
-    
--- Main.java
+++ Main.java
@@ -3,6 +3,14 @@ public class Main {
 
     public static void main (String [] args) throws InterruptedException{
         ClassA obj =  new ClassA();
+
+        //here we create two threads, each of them access the method of ClassA.
+        // in method of classA  we increment a variable  by 1, 100 times in a loop
+        // by using synchronized keyword in front of the method, we ensure that only 
+        // one thread at a time can access the  method
+        //  by doing so, we ensure the  end result of the variable will be 200
+        // if we didn't do it,  two threads  might have gotten the same update, 
+        //  and incease the varible simulatenously, 
         MultiThreading my_thing =  new MultiThreading(obj);
         MultiThreading my_thing2 =  new MultiThreading(obj);
         my_thing.start();
